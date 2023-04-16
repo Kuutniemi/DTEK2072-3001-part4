f0e1d9782b309cbdd611344f3cf8db32df9b4f4a
Kuutniemi <jkuutniemi@outlook.com>
2023-04-17 01:08:55 +0300

    4.6 done

--- tests/blogsapi.test.js
+++ tests/blogsapi.test.js
@@ -5,11 +5,30 @@ const app = require("../app");
 const api = supertest(app);
 
 test("blogs are returned as json and there's right amount of them", async () => {
+  // En jaksanut tehdä toista apia, jossa uudet blogit testaamista varten
+  const blogs = await api.get("/api/blogs");
+  const length = blogs.length;
+
   await api
     .get("/api/blogs")
     .expect(200)
     .expect("Content-Type", /application\/json/)
-    .expect((res) => res.length === 7);
+    .expect((res) => res.length === length);
+});
+
+test("Test adding blogs work and the amount of blogs increases", async () => {
+  const blogs = await api.get("/api/blogs");
+  let length = blogs.length;
+
+  const newBlog = {
+    title: "Test blog",
+    author: "Test author",
+    url: "Test url",
+    likes: 0,
+  };
+  await api.post("/api/blogs").send(newBlog).expect(201);
+  const blogss = await api.get("/api/blogs");
+  expect(blogss.length === length + 1);
 });
 
 afterAll(() => {
